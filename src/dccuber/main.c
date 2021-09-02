384f14c376b9409ca2b713bd63f88a317bc218a2
Sebastian Arriagada <snarriagada@uc.cl>
2021-09-02 17:20:20 -0400

    print

--- src/dccuber/main.c
+++ src/dccuber/main.c
@@ -29,5 +29,15 @@ int main(int argc, char const *argv[])
   printf("\n");
 
   printf("Liberando memoria...\n");
+
+  int distancia1 = data_in->lines[0][0];
+  int distancia2 = data_in->lines[0][1];
+  int distancia3 = data_in->lines[0][2];
+  printf(distancia1);
+  printf(distancia2);
+  printf(distancia3);
+
+
+
   input_file_destroy(data_in);
 }
