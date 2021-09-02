d7720831bec5fd2b510e74a9d42d8ff0c83baa94
Sebastian Arriagada <snarriagada@uc.cl>
2021-09-02 18:01:15 -0400

    leer inputs

--- src/dccuber/main.c
+++ src/dccuber/main.c
@@ -7,7 +7,7 @@ int main(int argc, char const *argv[])
 {
   printf("I'm the DCCUBER process and my PID is: %i\n", getpid());
 
-  char *filename = "input.txt";
+  char *filename = argv[1];
   InputFile *data_in = read_file(filename);
 
   printf("Leyendo el archivo %s...\n", filename);
@@ -46,12 +46,40 @@ int main(int argc, char const *argv[])
   int tiempo2 = atoi(data_in->lines[1][3]);
   int tiempo3 = atoi(data_in->lines[1][4]);
 
-  printf("%d", tiempo_de_creacion);
-  printf("%d", envios_necesarios);
-  printf("%d", tiempo1);
-  printf("%d", tiempo2);
-  printf("%d", tiempo3);
+  printf("%d \n", tiempo_de_creacion);
+  printf("%d \n", envios_necesarios);
+  printf("%d \n", tiempo1);
+  printf("%d \n", tiempo2);
+  printf("%d \n", tiempo3);
 
   input_file_destroy(data_in);
+
+  // ----- FORK
+
+  int id_parent = getpid();
+  printf("id parent: %d \n", id_parent);
+  pid_t pid;
+
+  /* fork a child process */
+  pid = fork();
+
+  if (pid < 0) { 
+    /* error occurred */
+    fprintf(stderr, "Fork Failed"); 
+    return 1;
+  }
+  else if (pid == 0) { 
+    /* child process */
+    printf("Child about to do exec\n"); 
+    printf("id child: %d \n", getpid());
+    execlp("/bin/ls","ls",NULL);
+    printf("Child done with exec\n");
+  }
+  else {
+    /* parent process */
+    /* parent will wait for the child to complete */
+    wait(NULL);
+    printf("Child Complete\n");
+  }
 }
 
