1f905defa8aad5cc18791d49512828c0a7130d7e
Sebastian Arriagada <snarriagada@uc.cl>
2021-09-06 12:40:13 -0300

    asd

--- src/semaforo/main.c
+++ src/semaforo/main.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
   printf("I'm the SEMAFORO process an,d my PID is: %i\n", getpid());
   //send_signal_with_int(atoi(argv[2]), 99);
   //int delay = strtol(argv[1], NULL, 10);
-  while (true)
+  while (1)
   {
     send_signal_with_int(atoi(argv[2]), atoi(argv[0]));
     sleep(atoi(argv[1]));
