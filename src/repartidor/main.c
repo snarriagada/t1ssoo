08ed20341c73ea22891497079f6af2bd978f58e8
Sebastian Arriagada <snarriagada@uc.cl>
2021-09-06 21:36:46 -0300

    reps

--- src/repartidor/main.c
+++ src/repartidor/main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char const *argv[])
 {
-  printf("dist1: %s dist2: %s dist3: %s distBodega:\n", argv[0], argv[1], argv[2], argv[3]);
+  printf("dist1: %s dist2: %s dist3: %s distBodega:%s\n", argv[0], argv[1], argv[2], argv[3]);
   printf("I'm the REPARTIDOR process and my PID is: %i\n", getpid());
 
   while (1)
